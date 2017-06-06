60f4e20b27408b3ae856651ad8ac7cd93a061559
Unknown <deemarashdan@gmail.com>
2017-06-06 15:59:31 -0400

    creating a new class

---
+++ src/pp.java
@@ -0,0 +1,21 @@
+
+public class pp {
+	int data;
+	String name;
+
+	public void nameMethod()
+	{
+	}
+
+	public void dataMethod()
+	{
+	}
+
+	@Override
+	public String toString()
+	{
+	    return name+data;
+	}
+
+
+}
